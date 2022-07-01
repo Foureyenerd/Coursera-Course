98482454685a22e291d5e50b3bc05dc23c91cc5b
Abdulsobur Sadiq <sadiqabdulsobur@gmail.com>
2022-07-01 15:34:12 -0500

    Created the folder name Example_4 and in it contains some html,css and js file

---
+++ Coursera_Javascript/Example_4/js/Script.js
@@ -0,0 +1,7 @@
+function updateFirst() {
+  document.getElementById("stuff").innerHTML = "First Button clicked";
+}
+
+function updateSecond() {
+  document.getElementById("stuff").innerHTML = "Second Button clicked";
+}
