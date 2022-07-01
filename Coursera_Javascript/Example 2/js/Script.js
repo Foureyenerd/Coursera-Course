35a1922a5772fe0705318b83adb329b603fb288f
Abdulsobur Sadiq <sadiqabdulsobur@gmail.com>
2022-07-01 13:54:15 -0500

    editited the script.js in example 2 folder

--- Coursera_Javascript/Example 2/js/Script.js
+++ Coursera_Javascript/Example 2/js/Script.js
@@ -2,8 +2,8 @@ var name1;
 
 // name1 = 17 % 5;
 
-name1 = prompt("Whats your name");
+name1 = prompt("What's your name?");
 
-// document.write(name1);
+document.write(name1);
 
 // console.log(name1);
